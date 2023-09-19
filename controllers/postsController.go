ddb963f11bd0df67f16ef377bc45c3a3d3047f5b
Jamshid Yuldashev <jamshidbek.yoldoshev.99@gmail.com>
2023-09-19 21:45:08 +0500

    complited CRUD api actions with all necessary details

--- controllers/postsController.go
+++ controllers/postsController.go
@@ -13,8 +13,20 @@ func Index(c *gin.Context) {
 }
 
 func CreatePost(c *gin.Context) {
-	post := models.Post{Title: "Title-1", Body: "Body-1"}
 
+	// create body struct
+	var body struct {
+		Title string
+		Body  string
+	}
+
+	//bind all data to body struct
+	c.Bind(&body)
+
+	// make Post object and fill it with given data
+	post := models.Post{Title: body.Title, Body: body.Body}
+
+	// create instance in db
 	result := initializers.DB.Create(&post) // pass pointer of data to Create
 
 	if result.Error != nil {
@@ -22,7 +34,85 @@ func CreatePost(c *gin.Context) {
 		return
 	}
 
+	// return response post data with status 200
 	c.JSON(200, gin.H{
 		"post": post,
+	})
+}
+
+func IndexPost(c *gin.Context) {
+
+	// create array consist of instance of models.Post struct
+	var posts []models.Post
+
+	//get all post data
+	initializers.DB.Find(&posts) // pass pointer of data to get
+
+	// return response post data with status 200
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+}
+
+func ShowPost(c *gin.Context) {
+
+	// get id param from request
+	id := c.Param("id")
+
+	// create instance of models.Post struct
+	var post models.Post
+
+	// get one Post data by id
+	initializers.DB.First(&post, id) // pass pointer of data to get
+
+	// return response post data with status 200
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func UpdatePost(c *gin.Context) {
+
+	// get id param from request
+	id := c.Param("id")
+
+	// create body struct
+	var body struct {
+		Body  string
+		Title string
+	}
+
+	//bind all data to body struct
+	c.Bind(&body)
+
+	// create instance of models.Post struct
+	var post models.Post
+
+	// get one Post data by id
+	initializers.DB.First(&post, id)
+
+	// update this instance with new data
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: body.Title,
+		Body:  body.Body,
+	})
+
+	// return response post data with status 200
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func DeletePost(c *gin.Context) {
+	// get id param from request
+	id := c.Param("id")
+
+	// delete instane in posts
+
+	initializers.DB.Delete(&models.Post{}, id)
+
+	// return response post data with status 200
+	c.JSON(200, gin.H{
+		"message": "Deleted successfully",
 	})
 }
--- router/routes.go
+++ router/routes.go
@@ -7,8 +7,11 @@ import (
 
 func Routes() {
 	router := gin.Default()
-	router.GET("/", controllers.Index)
+	router.GET("/post", controllers.IndexPost)
 	router.POST("/post", controllers.CreatePost)
+	router.GET("/post/:id", controllers.ShowPost)
+	router.PUT("/post/:id", controllers.UpdatePost)
+	router.DELETE("/post/:id", controllers.DeletePost)
 
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
